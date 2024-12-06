c9b7d44e37f76ffd090065700e0adfce65f9d867
Maksim Berdnikovich <maksim.berdnikovich@aventusit.eu>
2024-12-06 12:27:04 +0300

    project

--- project/src/layouts/Pages/Movie.js
+++ project/src/layouts/Pages/Movie.js
@@ -1,7 +1,7 @@
 import {useParams} from "react-router-dom";
 import MovieDetails from "../../components/Movies/MovieDetails";
 
-import movies from "../../movies.json";
+import movies from "../../data.json";
 import NotFound from "./NotFound";
 
 const Movie = () => {
